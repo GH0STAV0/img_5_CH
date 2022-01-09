663e71621597512c659ad1754233e9298edbea11
GH0STAV0 <you@example.com>
2022-01-09 01:06:08 -0800

    add new project

--- src/go_go.sh
+++ src/go_go.sh
@@ -4,15 +4,15 @@ trap "echo oh;exit" SIGTERM SIGINT
 
 echo -e "nameserver 103.86.96.100\nnameserver 103.86.99.100" >  /etc/resolv.conf
 cd /root/SDA_ALL/
-rm -rf site_5
+rm -rf site_5_cheap
 git reset --hard
 git pull
-cd /root/SDA_ALL/site_5/ && cp /root/0nord_pass /root/SDA_ALL/site_5/0nord_pass
+cd /root/SDA_ALL/site_5_cheap/ && cp /root/0nord_pass /root/SDA_ALL/site_5_cheap/0nord_pass
 chmod +x *
 pwd
 while true
 do
 	echo "NEW ..............."
-	cd /root/SDA_ALL/site_5/
+	cd /root/SDA_ALL/site_5_cheap/
 	timeout 5m python3 48_ads.py
 done
